d66bbfd2b7a35cacfb1470c8e1d897620eb01514
showqi <mshowqi12@gmail.com>
2023-10-12 20:38:13 +0600

    Add project files.

---
+++ shopmgmt/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace shopmgmt
+{    
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection conn = new SqlConnection("Data Source=DAXTER899\\SQLEXPRESS;Initial Catalog=mobile;Integrated Security=True");
+        string messgae = "Done";
+        string errorMessage = "Something went wrong";
+        
+        private void Submit(object sender, EventArgs e)
+        {
+            try
+            {
+                conn.Open();
+                var insertQuery = "insert into registrantion values(@username,@email,@phone,@password,@type)";
+                SqlCommand cmd = new SqlCommand(insertQuery, conn);
+                cmd.Parameters.AddWithValue("@username", Username_r.Text);
+                cmd.Parameters.AddWithValue("@email", email_r.Text);
+                cmd.Parameters.AddWithValue("@phone", phone_r.Text);
+                cmd.Parameters.AddWithValue("@password", pass_r.Text);
+                cmd.Parameters.AddWithValue("@type", comboBox1.SelectedItem);
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                Username_r.Clear();
+                email_r.Clear();
+                phone_r.Clear();
+                pass_r.Clear();
+                comboBox1.Items.Clear();
+                MessageBox.Show(messgae);
+            }
+            catch {
+
+                MessageBox.Show(errorMessage);
+            
+            }
+           
+
+        }
+
+        private void Login(object sender, EventArgs e)
+        {
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("select * from registrantion where username=@username and password = @password", conn);
+            cmd.Parameters.AddWithValue("@username", login_name.Text);
+            cmd.Parameters.AddWithValue("@phone",login_name.Text);
+            cmd.Parameters.AddWithValue("@password",login_pass.Text);
+            if (login_name.Text== "")
+            {
+                MessageBox.Show("Enter Username");
+
+            }
+            else if (login_pass.Text == "")
+            {
+                MessageBox.Show("Enter password");
+            }
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(cmd);
+            DataTable dataTable = new DataTable();
+            dataAdapter.Fill(dataTable);
+            if (dataTable.Rows.Count > 0)
+            {
+                MessageBox.Show(messgae);
+                login_name.Clear();
+                login_pass.Clear();
+                Home home = new Home();
+                home.Show();
+                home.Close();
+                
+            }
+            else
+            {
+                MessageBox.Show("invalid username or password");
+            }
+
+
+        }
+
+        
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+      
+
+        
+    }
+}
---
+++ shopmgmt/Home.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace shopmgmt
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.addMobile = new System.Windows.Forms.Button();
+            this.customerInfo = new System.Windows.Forms.Button();
+            this.stock = new System.Windows.Forms.Button();
+            this.sellInformation = new System.Windows.Forms.Button();
+            this.logOut = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Aqua;
+            this.label1.Location = new System.Drawing.Point(635, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 31);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "HOME";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(56, 134);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(104, 123);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(56, 547);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(104, 123);
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(826, 336);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(104, 123);
+            this.pictureBox3.TabIndex = 11;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(826, 134);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(104, 123);
+            this.pictureBox4.TabIndex = 12;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(56, 336);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(104, 123);
+            this.pictureBox5.TabIndex = 13;
+            this.pictureBox5.TabStop = false;
+            // 
+            // addMobile
+            // 
+            this.addMobile.BackColor = System.Drawing.Color.Orange;
+            this.addMobile.Location = new System.Drawing.Point(56, 274);
+            this.addMobile.Name = "addMobile";
+            this.addMobile.Size = new System.Drawing.Size(104, 28);
+            this.addMobile.TabIndex = 14;
+            this.addMobile.Text = "ADD MOBILE";
+            this.addMobile.UseVisualStyleBackColor = false;
+            this.addMobile.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // customerInfo
+            // 
+            this.customerInfo.BackColor = System.Drawing.Color.Orange;
+            this.customerInfo.Location = new System.Drawing.Point(56, 693);
+            this.customerInfo.Name = "customerInfo";
+            this.customerInfo.Size = new System.Drawing.Size(104, 28);
+            this.customerInfo.TabIndex = 15;
+            this.customerInfo.Text = "Customer info";
+            this.customerInfo.UseVisualStyleBackColor = false;
+            this.customerInfo.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // stock
+            // 
+            this.stock.BackColor = System.Drawing.Color.Orange;
+            this.stock.Location = new System.Drawing.Point(56, 483);
+            this.stock.Name = "stock";
+            this.stock.Size = new System.Drawing.Size(104, 28);
+            this.stock.TabIndex = 16;
+            this.stock.Text = "Stock";
+            this.stock.UseVisualStyleBackColor = false;
+            this.stock.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // sellInformation
+            // 
+            this.sellInformation.BackColor = System.Drawing.Color.Orange;
+            this.sellInformation.Location = new System.Drawing.Point(826, 274);
+            this.sellInformation.Name = "sellInformation";
+            this.sellInformation.Size = new System.Drawing.Size(104, 28);
+            this.sellInformation.TabIndex = 17;
+            this.sellInformation.Text = "Sell Information";
+            this.sellInformation.UseVisualStyleBackColor = false;
+            // 
+            // logOut
+            // 
+            this.logOut.BackColor = System.Drawing.Color.Orange;
+            this.logOut.Location = new System.Drawing.Point(826, 483);
+            this.logOut.Name = "logOut";
+            this.logOut.Size = new System.Drawing.Size(104, 28);
+            this.logOut.TabIndex = 18;
+            this.logOut.Text = "Log out";
+            this.logOut.UseVisualStyleBackColor = false;
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(1358, 795);
+            this.Controls.Add(this.logOut);
+            this.Controls.Add(this.sellInformation);
+            this.Controls.Add(this.stock);
+            this.Controls.Add(this.customerInfo);
+            this.Controls.Add(this.addMobile);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Home";
+            this.Text = "Home";
+            this.Load += new System.EventHandler(this.Home_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Button addMobile;
+        private System.Windows.Forms.Button customerInfo;
+        private System.Windows.Forms.Button stock;
+        private System.Windows.Forms.Button sellInformation;
+        private System.Windows.Forms.Button logOut;
+    }
+}
